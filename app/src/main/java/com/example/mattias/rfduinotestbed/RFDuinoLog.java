468e17d817d52e269329f5e0064655c3dc82a2aa
sewalliniusm <mattias.wallinius@adevoconsult.se>
2015-06-07 10:18:02 +0200

    First checkin

---
+++ app/src/main/java/com/example/mattias/rfduinotestbed/RFDuinoLog.java
@@ -0,0 +1,335 @@
+package com.example.mattias.rfduinotestbed;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanSettings;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.UUID;
+
+
+public class RFDuinoLog extends ActionBarActivity implements BluetoothAdapter.LeScanCallback{
+    // State machine
+    final private static int STATE_BLUETOOTH_OFF = 1;
+    final private static int STATE_DISCONNECTED = 2;
+    final private static int STATE_CONNECTING = 3;
+    final private static int STATE_CONNECTED = 4;
+    public static final int REQUEST_ENABLE_BT = 10;
+
+    private int state;
+
+    private boolean scanStarted;
+    private boolean scanning;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothDevice bluetoothDevice;
+
+    private RFduinoService rfduinoService;
+
+    private Button enableBluetoothButton;
+    private TextView scanStatusText;
+    private Button scanButton;
+    private TextView deviceInfoText;
+    private TextView connectionStatusText;
+    private Button connectButton;
+    private EditData valueEdit;
+    private Button sendZeroButton;
+    private Button sendValueButton;
+    private Button clearButton;
+    private LinearLayout dataLayout;
+    private TextView dataValue;
+
+    private final BroadcastReceiver bluetoothStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, 0);
+            if (state == BluetoothAdapter.STATE_ON) {
+                upgradeState(STATE_DISCONNECTED);
+            } else if (state == BluetoothAdapter.STATE_OFF) {
+                downgradeState(STATE_BLUETOOTH_OFF);
+            }
+        }
+    };
+
+    private final BroadcastReceiver scanModeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            scanning = (bluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_NONE);
+            scanStarted &= scanning;
+            updateUi();
+        }
+    };
+
+    private final BroadcastReceiver dataReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, final Intent intent) {
+            RFDuinoLog.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Bundle data = intent.getExtras();
+                    byte[] datas = data.getByteArray(RFduinoService.EXTRA_DATA);
+                    ByteBuffer buffer = ByteBuffer.wrap(datas);
+                    buffer.order(ByteOrder.LITTLE_ENDIAN);
+                 
+                    float temp = buffer.getFloat();
+                    dataValue.setText(Float.toString(temp));
+                }
+            });
+
+        }
+    };
+
+    private final ServiceConnection rfduinoServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            rfduinoService = ((RFduinoService.LocalBinder) service).getService();
+            if (rfduinoService.initialize()) {
+                if (rfduinoService.connect(bluetoothDevice.getAddress())) {
+                    upgradeState(STATE_CONNECTED);
+                }
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            rfduinoService = null;
+            downgradeState(STATE_DISCONNECTED);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==REQUEST_ENABLE_BT){
+            if(resultCode==RESULT_OK){
+                upgradeState(STATE_DISCONNECTED);
+                updateUi();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rfduino_log);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        // Bluetooth
+        enableBluetoothButton = (Button) findViewById(R.id.enableBluetooth);
+        enableBluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enableBluetoothButton.setEnabled(false);
+                if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }else{
+                    scanButton.setEnabled(true);
+                }
+                enableBluetoothButton.setText(
+                        bluetoothAdapter.enable() ? "Enabling bluetooth..." : "Enable failed!");
+            }
+        });
+
+        // Find Device
+        scanStatusText = (TextView) findViewById(R.id.scanStatus);
+
+        scanButton = (Button) findViewById(R.id.scan);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanStarted = true;
+                bluetoothAdapter.startLeScan(
+                        new UUID[]{RFduinoService.UUID_SERVICE },
+                        RFDuinoLog.this);
+            }
+        });
+
+        // Device Info
+        deviceInfoText = (TextView) findViewById(R.id.deviceInfo);
+
+        // Connect Device
+        connectionStatusText = (TextView) findViewById(R.id.connectionStatus);
+
+        connectButton = (Button) findViewById(R.id.connect);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setEnabled(false);
+                connectionStatusText.setText("Connecting...");
+                Intent rfduinoIntent = new Intent(RFDuinoLog.this, RFduinoService.class);
+                bindService(rfduinoIntent, rfduinoServiceConnection, BIND_AUTO_CREATE);
+            }
+        });
+
+        // Send
+        valueEdit = (EditData) findViewById(R.id.value);
+        valueEdit.setImeOptions(EditorInfo.IME_ACTION_SEND);
+        valueEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    sendValueButton.callOnClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        sendZeroButton = (Button) findViewById(R.id.sendZero);
+        sendZeroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rfduinoService.send(new byte[]{0});
+            }
+        });
+
+        sendValueButton = (Button) findViewById(R.id.sendValue);
+        sendValueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rfduinoService.send(valueEdit.getData());
+            }
+        });
+
+        // Receive
+        clearButton = (Button) findViewById(R.id.clearData);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataLayout.removeAllViews();
+            }
+        });
+
+        dataLayout = (LinearLayout) findViewById(R.id.dataLayout);
+        dataValue = (TextView) findViewById(R.id.dataValue);
+
+        if(bluetoothAdapter!=null && bluetoothAdapter.isEnabled()){
+            enableBluetoothButton.setEnabled(false);
+            scanButton.setEnabled(true);
+            upgradeState(STATE_DISCONNECTED);
+        }
+
+        IntentFilter dataIntentFilter = new IntentFilter(RFduinoService.ACTION_DATA_AVAILABLE);
+
+        // Registers the receiver with the new filter
+        this.registerReceiver(dataReceiver, dataIntentFilter);
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_rfduino_log, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLeScan(BluetoothDevice device, final int rssi, final byte[] scanRecord) {
+        bluetoothAdapter.stopLeScan(this);
+        bluetoothDevice = device;
+
+        RFDuinoLog.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                deviceInfoText.setText(
+                        BluetoothHelper.getDeviceInfoText(bluetoothDevice, rssi, scanRecord));
+                updateUi();
+            }
+        });
+
+    }
+
+    private void upgradeState(int newState) {
+        if (newState > state) {
+            updateState(newState);
+        }
+    }
+
+    private void downgradeState(int newState) {
+        if (newState < state) {
+            updateState(newState);
+        }
+    }
+
+    private void updateState(int newState) {
+        state = newState;
+        updateUi();
+    }
+
+    private void updateUi() {
+        // Enable Bluetooth
+        boolean on = state > STATE_BLUETOOTH_OFF;
+        enableBluetoothButton.setEnabled(!on);
+        enableBluetoothButton.setText(on ? "Bluetooth enabled" : "Enable Bluetooth");
+        scanButton.setEnabled(on);
+
+        // Scan
+        if (scanStarted && scanning) {
+            scanStatusText.setText("Scanning...");
+            scanButton.setText("Stop Scan");
+            scanButton.setEnabled(true);
+        } else if (scanStarted) {
+            scanStatusText.setText("Scan started...");
+            scanButton.setEnabled(false);
+        } else {
+            scanStatusText.setText("");
+            scanButton.setText("Scan");
+            scanButton.setEnabled(true);
+        }
+
+        // Connect
+        boolean connected = false;
+        String connectionText = "Disconnected";
+        if (state == STATE_CONNECTING) {
+            connectionText = "Connecting...";
+        } else if (state == STATE_CONNECTED) {
+            connected = true;
+            connectionText = "Connected";
+        }
+        connectionStatusText.setText(connectionText);
+        connectButton.setEnabled(bluetoothDevice != null && state == STATE_DISCONNECTED);
+
+        // Send
+        sendZeroButton.setEnabled(connected);
+        sendValueButton.setEnabled(connected);
+    }
+
+}
